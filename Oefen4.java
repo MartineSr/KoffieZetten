bd21a8ebaeb7a3e954745da86cd3b6c6b46e5750
MartineSr <martineschroor@gmail.com>
2018-05-30 16:16:51 +0200

    pruttel pruttel

--- Oefen4.java
+++ Oefen4.java
@@ -6,10 +6,9 @@ public class Oefen4 {
 	public static void main(String[] args) {
 		Persoon Martine = new Persoon();
 		Persoon Timon = new Persoon();
-		Ober ober = new Ober(); 
-				 
-		
-		
+		Apparaat apparaat = new Apparaat();		
+
+		Ober ober = new Ober(apparaat); 
 		
 		Martine.naam = "Martine";
 		Timon.naam = "Timon";
@@ -20,18 +19,38 @@ public class Oefen4 {
 class Apparaat{
 	int Lwater;
 	String[] Soorten = new String[] {"Ristretto","Espresso","Cafe Creme","Capuccino","Latte Macchiato"};
-	void ZetKoffie() {}
+	void ZetKoffie(String soort) {
+		System.out.println("Koffiezetapparaat: 'Pruttel pruttel'");
+	}
 }
 
 class Ober{
-	Ober(){
+	Apparaat mijnApparaat;
+	Ober(Apparaat apparaat){
+		mijnApparaat = apparaat;
 		BestellingOpnemen();
 	}
 	Scanner scanner = new Scanner(System.in);
 	void BestellingOpnemen() {
-		System.out.println("Wat wilt u drinken?");
+		System.out.println("Ober: 'Wat voor soort koffie wilt u drinken?'");
+		for(String s : mijnApparaat.Soorten) {
+			System.out.print(s);
+			if(!(s.equals("Latte Macchiato"))) {
+				System.out.print(" - ");
+			}else {
+				System.out.println(".");
+			}
+		}
 		String drinken = scanner.nextLine(); 
-		System.out.println(drinken);
+		
+		if(drinken.equals(mijnApparaat.Soorten[0])||drinken.equals(mijnApparaat.Soorten[1])||drinken.equals(mijnApparaat.Soorten[2])||drinken.equals(mijnApparaat.Soorten[3])||drinken.equals(mijnApparaat.Soorten[4])) {
+			System.out.println("Ober: 'Uw bestelling wordt verwerkt.'");
+			this.lopen("koffiezetapparaat");
+			mijnApparaat.ZetKoffie(drinken);
+		}else {
+			System.out.println("Ober: 'Dit soort koffie hebben wij niet.'");
+			BestellingOpnemen();
+		}
 		
 	}
 	
